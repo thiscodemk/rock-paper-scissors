d3cec2002730ebce304ac20066ff76ee6b37a726
Michael Kalusche <michaelkalusche92@gmail.com>
2022-06-29 06:46:15 +0200

    working version of rock,paper,scissors for one single round

--- rps.js
+++ rps.js
@@ -1,7 +1,37 @@
-function computerPlay(randomItem) {
-    const items = ['Schere','Papier','Stein'];
+function computerPlay() {
+    const items = ['scissors','paper','rock'];
     var randomItem = items[Math.floor(Math.random()*items.length)];
-    console.log(randomItem);
+    return (randomItem);
   }
 
 computerPlay();
+
+//Computer Wert in einer Variable speichern
+const computerSelection = computerPlay().toLowerCase();
+
+//Funktion zur Auswahl durch den Spieler
+const playerSelection = prompt("Scissors, Paper or Rock?").toLowerCase();
+
+console.log(computerSelection);
+console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return("Unentschieden");
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return ("Papier umwickelt den Stein, leider verloren");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("Papier umwickelt den Stein, Du gewinnst");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return ("Stein zerstört Schere, Du verlierst");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return ("Stein zerstört Schere, Du gewinnst");
+    }
+  }
+  
+  console.log(playRound(playerSelection, computerSelection));
+
